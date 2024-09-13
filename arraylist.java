fbfd264f633ff9e3f8c3767bd100f84a268e99eb
Sangram Zanje <89483744+sangramzanje@users.noreply.github.com>
2024-09-13 13:45:15 +0530

    Update arraylist.java

--- arraylist.java
+++ arraylist.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class Main { 
   public static void main(String[] args) { 
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("sangram");
-    cars.add("abhijit");
-    cars.add("prajwal");
-    cars.add("rathod");
-    System.out.println(cars);
+    ArrayList<String> name = new ArrayList<String>();
+    name.add("sangram");
+    name.add("abhijit");
+    name.add("prajwal");
+    name.add("rathod");
+    System.out.println(name);
   } 
 }
