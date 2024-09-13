54fd4d1fcab4e2997f045eaeaf3a750797d02621
Sangram Zanje <89483744+sangramzanje@users.noreply.github.com>
2024-09-13 13:41:53 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("sangram");
+    cars.add("abhijit");
+    cars.add("prajwal");
+    cars.add("rathod");
+    System.out.println(cars);
+  } 
+}
---
+++ arraylist2.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    
+    cars.add(0, "Mazda"); 
+
+    System.out.println(cars);
+  } 
+}
